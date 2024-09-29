8008f5f05e6740682ef317c6edc1792056f9b21f
bskdany <54674556+bskdany@users.noreply.github.com>
2024-09-29 01:47:05 -0400

    rewrote the simple p2p file

--- backend/simple-p2p-chat.py
+++ backend/simple-p2p-chat.py
@@ -2,9 +2,9 @@ import socket
 import threading
 import json
 import time
-import uuid
 from os import listdir
 import hashlib
+import tokenizer
 
 DISCOVERY_PORT = 5000
 CHAT_PORT = 5001
@@ -12,9 +12,13 @@ CHAT_PORT = 5001
 existing_files = dict()
 
 
+def hash(input):
+    return hashlib.sha256(input.encode("utf-8")).hexdigest()
+
+
 class P2PClient:
     def __init__(self):
-        self.user_id = uuid.uuid1().__str__()
+        self.ip = socket.gethostbyname(socket.gethostname())
         self.peers = {}
         self.discovery_socket = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,69 +32,104 @@ class P2PClient:
         threading.Thread(target=self.discover_peers, daemon=True).start()
         threading.Thread(target=self.listen_for_messages, daemon=True).start()
         threading.Thread(target=self.announce_presence, daemon=True).start()
-        # self.announce_presence()
-        self.chat_loop()
 
     def discover_peers(self):
         while True:
             data, addr = self.discovery_socket.recvfrom(1024)
             message = json.loads(data.decode())
-            if message['type'] == 'announce' and message['user_id'] != self.user_id:
-                self.peers[message['user_id']] = addr[0]
-                # print(f"Discovered peer: {message['user_id']} at {addr[0]}")
+            if message['type'] == 'announce' and message['ip'] != self.ip:
+                self.peers[message['ip']] = addr[0]
 
     def announce_presence(self):
         while True:
             message = json.dumps({
                 'type': 'announce',
-                'user_id': self.user_id
+                'ip': self.ip
             })
             self.discovery_socket.sendto(
                 message.encode(), ('192.168.211.255', DISCOVERY_PORT))
             time.sleep(2)
 
+    # Ask all discovered peers for the file fingerprint
+    def request_file_fingerprint(self, message):
+        file_id = message["file_id"]
+
+        for ip in self.peers.values():
+            message = json.dumps({
+                'origin_ip': self.ip,
+                'type': 'request_file_fingerprint',
+                'file_id': file_id
+            })
+            self.chat_socket.sendto(message.encode(), (ip, CHAT_PORT))
+
+    # Answering the file fingeprint request
+    def response_file_fingerprint(self, message):
+        file_id = message["file_id"]
+        caller_ip = message["origin_ip"]
+        if (file_id in existing_files):
+            file_fingerprint_name = './sources/' + existing_files[file_id]
+            with open(file_fingerprint_name, "r") as f:
+
+                message = json.dumps({
+                    'origin_ip': self.ip,
+                    'type': 'reponse_file_fingerprint',
+                    'content': f.read()
+                })
+                self.chat_socket.sendto(
+                    message.encode(), (caller_ip, CHAT_PORT))
+
+    # Ask all discovered peers for the block data
+    def request_block(self, message):
+        file_id = message["file_id"]
+        block_index = message["block_index"]
+
+        # asking all peers for the file_id
+        # TODO make it so that only one returns it at the time
+        for ip in self.peers.values():
+            message = json.dumps({
+                'origin_ip': self.ip,
+                'type': 'request_block',
+                'file_id': file_id,
+                'block_index': block_index
+            })
+            self.chat_socket.sendto(message.encode(), (ip, CHAT_PORT))
+
+    def response_block(self, message):
+        file_id = message["file_id"]
+        block_index = message["block_index"]
+        target_file_name = existing_files[file_id][0]
+
+        block_data = tokenizer.get_block_content(
+            "./sources/" + target_file_name, block_index)
+
+        message = json.dumps({
+            'origin_ip': self.ip,
+            'type': 'respond_block',
+            'file_id': file_id,
+            'block_index': block_index,
+            'block_data': block_data
+        })
+
+        caller_ip = message["origin_ip"]
+
+        self.chat_socket.sendto(message.encode(), (caller_ip, CHAT_PORT))
+
     def listen_for_messages(self):
         while True:
             data, addr = self.chat_socket.recvfrom(1024)
-            print(type(data))
             message = json.loads(data.decode())
+            print(message)
 
-            # if (message["type"] == "get_file_hash":
-            #     with open
-            #
-
-            print(f"\n{message['from']}: {message['content']}")
-
-    def request_id(self, file_id):
-        for ip in self.peers.values():
-            message = json.dumps({
-                'from': self.user_id,
-                'content': file_id
-            })
-            self.chat_socket.sendto(message.encode(), (ip, CHAT_PORT))
-
-    def get_file_hash(self, file_id):
-        for ip in self.peers.values():
-            message = json.dumps({
-                'from': self.user_id,
-                'type': 'get_file_hash',
-                'content': file_id
-            })
-            self.chat_socket.sendto(message.encode(), (ip, CHAT_PORT))
-
-    def get_block_hash(self, block_hash):
-        for ip in self.peers.values():
-            message = json.dumps({
-                'from': self.user_id,
-                'type': 'get_block_hash',
-                'content': block_hash
-            })
-            self.chat_socket.sendto(message.encode(), (ip, CHAT_PORT))
-
-    def chat_loop(self):
-        while True:
-            file_id = input("Enter file id: ")
-            self.request_id(file_id)
+            if (message["type"] == "request_file_fingerprint"):
+                self.request_file_fingerprint(message)
+            elif (message["type"] == "request_block"):
+                self.request_block(message)
+            elif (message["type"] == "response_file_fingerprint"):
+                self.response_file_fingerprint(message)
+            elif (message["type"] == "response_block"):
+                self.response_block(message)
+            else:
+                print("Invalid message type: " + message["type"])
 
 
 if __name__ == "__main__":
@@ -100,8 +139,7 @@ if __name__ == "__main__":
             file_fingerprint_content = f.read()
             file_fingerprint_data = json.loads(file_fingerprint_content)
 
-            file_fingerprint_hash = hashlib.sha256(
-                file_fingerprint_content.encode("utf-8")).hexdigest()
+            file_fingerprint_hash = hash(file_fingerprint_content)
             target_file_name = file_fingerprint_data["header"]["file_name"]
 
             existing_files[file_fingerprint_hash] = [
