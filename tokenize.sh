114aa35acaf21f28e55289f34eb53797d3adcd4c
asterbot <arjunsodhi7@gmail.com>
2024-09-28 22:03:44 -0400

    cleaned up backend

--- tokenize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Tokenizes the file found in p2p_data
-
-if [ $# -ne 1 ]; then
-    echo "Usage: ./tokenize.sh [file in p2p_data]"
-    exit 1
-fi
-
-python tokenizer.py p2p_data/$1 > sources/${1%.txt}.hackthehill
