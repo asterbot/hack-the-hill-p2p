4b8838ffb9e6597f12d73a2a38d0a607bff74d1c
TheFJcurve <sargunbhatti27@gmail.com>
2024-11-08 17:03:39 -0500

    setting up for unit tests

---
+++ backend/test/test_file_tokenizer.py
@@ -0,0 +1,18 @@
+"""
+TODO
+"""
+
+import unittest
+
+
+class TestFileTokenizer(unittest.TestCase):
+    """
+    TODO
+    """
+
+    def test_dummy(self):
+        """
+        TODO
+        """
+
+        self.assertEqual(1 + 1, 2)
---
+++ backend/test/test_main.py
@@ -0,0 +1,18 @@
+"""
+TODO
+"""
+
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    """
+    TODO
+    """
+
+    def test_dummy(self):
+        """
+        TODO
+        """
+
+        self.assertEqual(1 + 1, 2)
