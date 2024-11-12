3db1f465da5f579bf2992adf34b0d409880b04e6
TheFJcurve <sargunbhatti27@gmail.com>
2024-11-11 20:04:47 -0500

    working on test_utils

--- backend/code/utils.py
+++ backend/code/utils.py
@@ -1,24 +1,37 @@
 """
-TODO
+Utilities Class
+
+Generic functions used throughout the project that don't belong to a particular class
 """
 
 import os
 import hashlib
 
 from pathlib import Path
+from typing import Optional
 
 
 def custom_hash(encode_input):
     """
-    TODO
+    Using the hashlib sha256 encoding, we specifically hash the utf-8 encoding for 
+    text files. This is our custom hash function we use in the entire project, we should not
+    use the inbuilt hash() function
     """
 
     return hashlib.sha256(encode_input.encode("utf-8")).hexdigest()
 
 
-def find_file(directory, filename):
+def find_file(directory: str, filename: str) -> Optional[str]:
     """
-    TODO
+    Helper function for get_filename_by_file_id
+    
+    Given a directory and a filename, check if the directory exists, and if the file
+    exists inside the given directory. If yes, return the file name, otherwise return
+    None
+    
+    :param directory: str
+    :param filename: str
+    :returns: Either string or None
     """
 
     if os.path.exists(directory):
--- backend/test/test_utils.py
+++ backend/test/test_utils.py
@@ -1,15 +1,24 @@
 """
-TODO
+Testing the Utilities class
 """
-
+import hashlib
 import unittest
-from code.utils import find_file
+from code.utils import find_file, custom_hash
 
 
 class TestUtils(unittest.TestCase):
     """
-    TODO
+    Utilities class functions are generic and widely used. Their implementation is very important.
     """
+    
+    def test_custom_hash(self):
+        """
+        We have our own implementation for hashing utf-8 format encoding
+        """
+        
+        encoded_input = "Hi Mom"
+        
+        self.assertEqual(custom_hash(encoded_input), hashlib.sha256(encoded_input.encode("utf-8")).hexdigest())
 
     def test_find_file_with_garbage_file_name_returns_none(self):
         """
@@ -24,3 +33,13 @@ class TestUtils(unittest.TestCase):
         """
 
         self.assertEqual(find_file("random_directory", "uploads.txt"), None)
+    
+    def test_find_file_with_proper_director_and_file_returns_filename(self):
+        """
+        If a file and directory exists, and file exists inside the directory, return the 
+        filename
+        """
+        
+        self.assertEqual(find_file("code", "utils"), "utils.py")
+        
+     
